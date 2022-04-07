c190e2f174b2699c033667e2c25e38e4e98724c5
Evgeniy Kozlov <jenekomail@gmail.com>
2022-04-07 14:21:29 +0300

    feat: add gulp config

---
+++ shelter/gulpfile.js
@@ -0,0 +1,43 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+
+const pug = require('gulp-pug');
+const htmlBeautify = require('gulp-html-beautify');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+
+function buildPug() {
+    return src('./src/pug/**/!(_)*.pug')
+        .pipe(pug())
+        .pipe(htmlBeautify())
+        .pipe(dest('./dist'))
+}
+
+function buildSass() {
+    return src('./src/scss/**/*.scss', { sourcemaps: true })
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('./dist/css', { sourcemaps: '.' }));
+}
+
+function copyAssets() {
+    return src('./src/assets/**/*.*')
+        .pipe(dest('./dist/assets'));
+}
+
+function copyFonts() {
+    return src('./src/fonts/**/*.*')
+        .pipe(dest('./dist/fonts'));
+}
+
+function clean() {
+    return del(['./dist']);
+}
+
+exports.clean = clean;
+exports.build = series(clean, parallel(buildPug, buildSass, copyAssets, copyFonts));
+exports.watch = function () {
+    exports.build();
+    watch('./src/scss/**/*.scss', buildSass);
+    watch('./src/**/*.pug', buildPug);
+    watch('./src/assets/**/*.*', copyAssets);
+    watch('./src/fonts/**/*.*', copyFonts);
+}
