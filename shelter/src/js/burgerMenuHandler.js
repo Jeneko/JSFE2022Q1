50d725131f8abd99ab7296f81c5cf3aae5a22231
Evgeniy Kozlov <jenekomail@gmail.com>
2022-04-19 21:05:44 +0300

    fix: split script.js into es modules

---
+++ shelter/src/js/burgerMenuHandler.js
@@ -0,0 +1,27 @@
+export function burgerMenuHandler() {
+    const body = document.body;
+    const burgerMenu = document.querySelector('.burger-menu');
+    const headerConent = document.querySelector('.header');
+
+    document.addEventListener('click', function (e) {
+        // Handle burger button and backdrop
+        if (e.target.closest('.burger-button') || e.target.closest('.backdrop')) {
+            toggleBurgerMenu();
+        }
+        // Handle anchor links
+        if (e.target.classList.contains('burger-menu__menu-link')) {
+            toggleBurgerMenu();
+        }
+    });
+
+    function toggleBurgerMenu() {
+        // Toggle burger-menu visibility
+        burgerMenu.classList.toggle('visible');
+        // Toggle header content visibility
+        headerConent.classList.toggle('hidden');
+        // Toggle scroll on body
+        body.classList.toggle('overflow-hidden');
+        // Rotate all burger-buttons
+        document.querySelectorAll('.burger-button').forEach(el => el.classList.toggle('rotate-90'));
+    }
+}
--- shelter/src/js/script.js
+++ shelter/src/js/script.js
@@ -1,31 +1,5 @@
+import { burgerMenuHandler } from './burgerMenuHandler.js';
+
 window.onload = () => {
     burgerMenuHandler();
-}
-
-function burgerMenuHandler() {
-    const body = document.body;
-    const burgerMenu = document.querySelector('.burger-menu');
-    const headerConent = document.querySelector('.header');
-
-    document.addEventListener('click', function (e) {
-        // Handle burger button and backdrop
-        if (e.target.closest('.burger-button') || e.target.closest('.backdrop')) {
-            toggleBurgerMenu();
-        }
-        // Handle anchor links
-        if (e.target.classList.contains('burger-menu__menu-link')) {
-            toggleBurgerMenu();
-        }
-    });
-
-    function toggleBurgerMenu() {
-        // Toggle burger-menu visibility
-        burgerMenu.classList.toggle('visible');
-        // Toggle header content visibility
-        headerConent.classList.toggle('hidden');
-        // Toggle scroll on body
-        body.classList.toggle('overflow-hidden');
-        // Rotate all burger-buttons
-        document.querySelectorAll('.burger-button').forEach(el => el.classList.toggle('rotate-90'));
-    }
 }
