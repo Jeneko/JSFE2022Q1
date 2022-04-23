d004ae655247b3987e17d3f951536607870809e8
Evgeniy Kozlov <jenekomail@gmail.com>
2022-04-23 23:58:29 +0300

    fix: add check to fix error when there is no header--fixed

--- shelter/src/js/burgerMenuHandler.js
+++ shelter/src/js/burgerMenuHandler.js
@@ -1,5 +1,6 @@
 export function burgerMenuHandler() {
     const body = document.body;
+    const headerFixed = document.querySelector('.header--fixed');
     const burgerMenu = document.querySelector('.burger-menu');
     const headerConent = document.querySelector('.header');
 
@@ -24,16 +25,20 @@ export function burgerMenuHandler() {
         // Toggle scroll on body
         // and fix horizontal shifting caused by hidden scroll
         if (burgerMenu.classList.contains('visible')) {
-            let prevWidth = document.body.getBoundingClientRect().width;
-            document.body.classList.add('overflow-hidden');
-            let curWidth = document.body.getBoundingClientRect().width;
+            let prevWidth = body.getBoundingClientRect().width;
+            body.classList.add('overflow-hidden');
+            let curWidth = body.getBoundingClientRect().width;
             let diffWidth = curWidth - prevWidth;
-            document.body.style.marginRight = diffWidth + 'px';
-            document.querySelector('.header--fixed').style.marginRight = diffWidth + 'px';
+            body.style.marginRight = diffWidth + 'px';
+            if (headerFixed) {
+                headerFixed.style.marginRight = diffWidth + 'px';
+            }
         } else {
-            document.body.classList.remove('overflow-hidden');
-            document.body.style.marginRight = '';
-            document.querySelector('.header--fixed').style.marginRight = '';
+            body.classList.remove('overflow-hidden');
+            body.style.marginRight = '';
+            if (headerFixed) {
+                document.querySelector('.header--fixed').style.marginRight = '';
+            }
         }
     }
 }
