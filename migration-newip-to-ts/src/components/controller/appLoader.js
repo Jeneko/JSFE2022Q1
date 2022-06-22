4e1b4a8e29782fbef9844f03cb99c93b0e7e38e0
Jeneko <jenekomail@gmail.com>
2022-06-22 19:35:48 +0300

    refactor: add apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0f0f19a5feca4a06b58dacd60aafec86', // получите свой ключ https://newsapi.org/
         });
     }
 }
