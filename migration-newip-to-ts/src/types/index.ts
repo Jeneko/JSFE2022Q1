f216c7e55123bf2c37842c34c77a2e97fd4840e6
Jeneko <jenekomail@gmail.com>
2022-06-23 20:36:30 +0300

    refactor: change tab size from 2 to 4 spaces

--- migration-newip-to-ts/src/types/index.ts
+++ migration-newip-to-ts/src/types/index.ts
@@ -3,47 +3,47 @@ import Sources from '../components/view/sources/sources';
 
 // App View
 export interface IAppView {
-  readonly news: News;
-  readonly sources: Sources;
+    readonly news: News;
+    readonly sources: Sources;
 
-  drawNews(data: NewsResponse): void;
-  drawSources(data: NewsResponse): void;
+    drawNews(data: NewsResponse): void;
+    drawSources(data: NewsResponse): void;
 }
 
 // Draw Data
 export interface IDrawData<T> {
-  draw(data: T): void;
+    draw(data: T): void;
 }
 
 // Response Data
 export type NewsResponse = {
-  readonly status: 'ok' | 'error';
-  readonly code?: string;
-  readonly message?: string;
-  readonly totalResults?: number;
-  readonly articles?: INewsArticle[];
-  readonly sources?: INewsSource[];
+    readonly status: 'ok' | 'error';
+    readonly code?: string;
+    readonly message?: string;
+    readonly totalResults?: number;
+    readonly articles?: INewsArticle[];
+    readonly sources?: INewsSource[];
 };
 
 // Article Data
 export interface INewsArticle {
-  readonly source: INewsSource;
-  readonly author: string;
-  readonly title: string;
-  readonly description: string;
-  readonly url: string;
-  readonly urlToImage: string;
-  readonly publishedAt: string;
-  readonly content: string;
+    readonly source: INewsSource;
+    readonly author: string;
+    readonly title: string;
+    readonly description: string;
+    readonly url: string;
+    readonly urlToImage: string;
+    readonly publishedAt: string;
+    readonly content: string;
 }
 
 // Source Data
 export interface INewsSource {
-  readonly id: string;
-  readonly name: string;
-  readonly description: string;
-  readonly url: string;
-  readonly category: string;
-  readonly language: string;
-  readonly country: string;
+    readonly id: string;
+    readonly name: string;
+    readonly description: string;
+    readonly url: string;
+    readonly category: string;
+    readonly language: string;
+    readonly country: string;
 }
