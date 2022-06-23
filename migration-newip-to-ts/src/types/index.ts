ba25202a46d32ba11c030d3d46c4a4dc846a429a
Jeneko <jenekomail@gmail.com>
2022-06-23 14:04:15 +0300

    feat: add interfaces for News API data

---
+++ migration-newip-to-ts/src/types/index.ts
@@ -0,0 +1,22 @@
+// Article Data
+export interface INewsArticle {
+  readonly source: INewsSource;
+  readonly author: string;
+  readonly title: string;
+  readonly description: string;
+  readonly url: string;
+  readonly urlToImage: string;
+  readonly publishedAt: string;
+  readonly content: string;
+}
+
+// Source Data
+export interface INewsSource {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly url: string;
+  readonly category: string;
+  readonly language: string;
+  readonly country: string;
+}
