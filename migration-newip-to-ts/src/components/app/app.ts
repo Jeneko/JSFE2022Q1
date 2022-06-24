351a93dc9f6493a3a7978aeb0b7b86bebdd9f4a7
Jeneko <jenekomail@gmail.com>
2022-06-24 20:23:50 +0300

    refactor: migrate 'class App' to .ts

---
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -0,0 +1,22 @@
+import { NewsResponse } from '../../types/index';
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    controller: AppController;
+    view: AppView;
+
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        (document.querySelector('.sources') as HTMLElement).addEventListener('click', (e) =>
+            this.controller.getNews(e, (data: NewsResponse) => this.view.drawNews(data))
+        );
+        this.controller.getSources((data: NewsResponse) => this.view.drawSources(data));
+    }
+}
+
+export default App;
