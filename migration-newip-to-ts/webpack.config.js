0ba78e96f085fb962016d093c4b5825453131ae4
Jeneko <jenekomail@gmail.com>
2022-06-24 22:04:10 +0300

    feat: add copy-webpack-plugin to webpack config

--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
+const copyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index'),
@@ -32,6 +33,11 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new EslingPlugin({ extensions: 'ts' }),
+        new copyPlugin({
+            patterns: [
+                { from: "src/images", to: "../dist/images" },
+            ]
+        }),
     ],
 };
 
