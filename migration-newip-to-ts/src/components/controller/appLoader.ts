0e3be93afa0aea5f7a07098727d5a98f4cec28c9
Jeneko <jenekomail@gmail.com>
2022-06-28 19:38:54 +0300

    refactor: replace API link to make API works on gh-pages

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '0f0f19a5feca4a06b58dacd60aafec86', // получите свой ключ https://newsapi.org/
         });
     }
